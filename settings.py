28fe60c4dce003c0ca0cc3435d02a06ceb4f17de
Nirmal AK <aknirmal90@gmail.com>
2018-05-11 03:01:31 +0530

    using factory websockets

---
+++ settings.py
@@ -0,0 +1,11 @@
+# Authorization keys for the Bitmex API / Websocket
+BITMEX_API_KEY = "vJqsBohymTHanzFJFvI6lj3Y"
+BITMEX_API_SECRET = "jSyzgy9wkHanxEml8mQU_d4SoiVtK82XO8Nhbw12WhqlTSXM"
+
+# URL to hit our API requests with
+BITMEX_BASE_URL = "https://www.bitmex.com/api/v1"
+
+# Multiplier to convert satoshi to XBT
+XBt_to_XBT = 100000000
+
+LOG_LEVEL = "DEBUG"
--- settings/settings_trades.py
+++
@@ -1,9 +0,0 @@
-# Authorization keys for the Bitmex API / Websocket
-BITMEX_API_KEY = "UxOXp8FEAZn6ImYTR0vexA6u"
-BITMEX_API_SECRET = "QuhRfqZoWOf2Bf2hv0W806VN3EorS_wCc69dMz_lHP-eItVW"
-
-# URL to hit our API requests with
-BITMEX_BASE_URL = "https://testnet.bitmex.com/api/v1"
-
-# Multiplier to convert satoshi to XBT
-XBt_to_XBT = 100000000
